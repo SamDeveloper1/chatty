5dfe13914b97a182a906964c7140af455cb00e92
Samarth Garg <samarthgarg108@gmail.com>
2026-02-19 17:02:02 +0530

    sidebar completed

--- frontend/src/components/Contactslist.jsx
+++ frontend/src/components/Contactslist.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
 
-const Contactslist = () => {
+const ChatsList = () => {
+  const { allContacts, isUsersLoading, setSelectedUser, getAllContacts } =
+    useChatStore();
+  useEffect(() => {
+    getAllContacts();
+  }, [getAllContacts]);
+  if (isUsersLoading) return <UsersLoadingSkeleton />;
   return (
-    <div>Contactslist</div>
-  )
-}
+    <>
+      {allContacts.map((contact) => (
+        <div
+          key={contact._id}
+          className="bg-cyan-500/10 p-10 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
+          onClick={() => setSelectedUser(contact)}
+        >
+          <div className="flex items-center gap-3">
+            <div className={`avatar online`}>
+              <div className="size-12 rounded-full">
+                <img
+                  src={contact.profilePic || "/avatar.png"}
+                  alt="chat.fullName"
+                />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">
+              {contact.fullName}
+            </h4>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default Contactslist
+export default ChatsList;
